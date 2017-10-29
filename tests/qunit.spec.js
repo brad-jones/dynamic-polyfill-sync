b1a0d75c93360e970a15227771175048caa780b7
Brad Jones <brad@bjc.id.au>
2017-10-29 21:43:25 +1100

    test(promise): add in a promise test case

--- tests/qunit.spec.js
+++ tests/qunit.spec.js
@@ -8,3 +8,9 @@ QUnit.test("EnsureSymbolPolyfillLoads", function(assert)
     window['@brad-jones/dynamic-polyfill-sync'](['Symbol']);
     assert.ok(Symbol);
 });
+
+QUnit.test("EnsurePromisePolyfillLoads", function(assert)
+{
+    window['@brad-jones/dynamic-polyfill-sync'](['Promise']);
+    assert.ok(Promise);
+});
